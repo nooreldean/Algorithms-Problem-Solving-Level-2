0b97bc357ed1ab496b379513b7761dafc0558b2f
nooreldean <nooreldean450@gmail.com>
2025-08-26 23:07:19 +0300

    added code

--- code/1.cpp
+++ code/1.cpp
@@ -2,11 +2,45 @@
 #include <string>
 #include <cmath>
 using namespace std;
+void printtableheader()
+{
+    cout << "\n----------------------------------------------------------------------------------------------------\n";
+    cout << "\n\n\t\t\t\t Multiplication Table From 1 to 10\n\n";
+    cout << "  \t";
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "\t";
+    }
+    cout << "\n----------------------------------------------------------------------------------------------------\n";
+}
+string columsparator(int i)
+{
+   if (i < 10)
+   {
+       return " |";
+   }
+   else
+   return " |";
+}
+void printmultiplicationtable()
+{
+    printtableheader();
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " |\t";
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << i * j << "\t";
+        }
+        cout << "\n";
+    }
+    cout << "\n----------------------------------------------------------------------------------------------------\n";
+}
 
 
 int main()
 {
-  
+    printmultiplicationtable();
     
     return 0;   
     
