1a32458301bffa18878b0f295cc02c1128614136
nooreldean <nooreldean450@gmail.com>
2025-10-03 18:22:26 +0300

    save 32

--- code/32.cpp
+++ code/32.cpp
@@ -2,48 +2,50 @@
 #include <string>
 #include <cmath>
 using namespace std;
-int randomenum(int from, int to)
+int randomnumber(int from, int to)
 {
-    int reednum = rand() % (to - from + 1) + from;
-    return reednum;
+    int randomnum = rand() % (to - from + 1) + from;
+    return randomnum;
 }
-void fillarraywithrandom(int arr[100],int arrlength)
+void fillarraywithrandomnumbers(int arr[100], int &arrlen)
 {
-
-    for(int i=0;i<arrlength;i++)
+    cout <<"enter number of elements:\n";
+    cin >> arrlen;
+    for (int i = 0; i < arrlen; i++)
     {
-        arr[i]=randomenum(1,100);
+        arr[i] = randomnumber(1, 100);
     }
 }
-void printarray(int arr[100],int arrlength)
+
+void printarray(int arr[100], int &arrlen)
 {
-    for(int i=0;i<arrlength;i++)
+    for (int i = 0; i < arrlen; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void printarrayidrevresnum(int arrsource[100], int arrdestination[100], int arrlength)
+void copyarrayreverseorder(int arr[100], int arrlen, int arr2[100])
 {
-    for(int i=0; i<arrlength; i++)
-        arrdestination[i] = arrsource[arrlength-1-i];
+    for (int i = 0; i < arrlen; i++)
+    {
+        arr2[i] = arr[arrlen - 1 - i];
+    }
 }
-
 
 int main()
 {
-   srand((unsigned)time(NULL));
-   int arr[100],arrlenght;
-   fillarraywithrandom(arr,arrlenght);
-   int arr2[100];
-   printarrayidrevresnum(arr,arr2,arrlenght);
-   cout<<"\narr 1 elements : \n";
-   printarray(arr,arrlenght);
-   cout<<"\narr 2 elements : \n";
-   printarray(arr2,arrlenght);
-  
-    
-    return 0;   
-    
+    srand((unsigned)time(NULL));
+    int arr[100], arrlen;
+    fillarraywithrandomnumbers(arr, arrlen);
+    int arr2[100];
+
+    copyarrayreverseorder(arr, arrlen, arr2);
+    cout << "original array:\n";
+    printarray(arr, arrlen);
+    cout << "reversed array:\n";
+    printarray(arr2, arrlen);
+    return 0;
 }
+
