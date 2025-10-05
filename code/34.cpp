971b8822e2691ba9ea21c24155c9bb4695c15977
nooreldean <nooreldean450@gmail.com>
2025-10-05 18:37:15 +0300

    save

--- code/34.cpp
+++ code/34.cpp
@@ -2,12 +2,88 @@
 #include <string>
 #include <cmath>
 using namespace std;
+int readpositivenumber(string message)
+{
+    cout<<endl;
+    int num=0;
+    do
+    {
+        
+        cout << message << "\n";
+
+        cin >> num;
+    }while(num<=0);
+    return num;
+}
+
+int randomnumber(int from, int to)
+{
+    int randomnum = rand() % (to - from + 1) + from;
+    return randomnum;
+}
+
+void fillarraywithrandomnumbers(int arr[100], int &arrlen)
+{
+    cout <<"enter number of elements:\n";
+    cin >> arrlen;
+    for (int i = 0; i < arrlen; i++)
+    {
+        arr[i] = randomnumber(1, 100);
+    }
+}
+
+void printarray(int arr[100], int &arrlen)
+{
+    for (int i = 0; i < arrlen; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int findnumberinarray(int arr[100], int &arrlen, int number)
+{
+   
+    for (int i = 0; i < arrlen; i++)
+    {
+        if(arr[i]==number)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int readnum()
+{
+    int num;
+    cout<<"please enter a num to search for:";
+    cin>>num;
+    return num;
+}
 
 
 int main()
 {
-  
-    
-    return 0;   
-    
+  srand((unsigned)time(NULL));
+    int arr[100], arrlen,number;
+    fillarraywithrandomnumbers(arr, arrlen);
+    cout << "array elements:\n";
+    printarray(arr, arrlen);
+   int num=readnum();
+   cout << "\nNumber you are looking for is: " << num << endl;
+    short NumberPosition = findnumberinarray(arr,arrlen,num);
+    if (NumberPosition==-1)
+    {
+        cout<<"Number not found :( ";
+    }
+
+    else
+    {
+         cout << "The number found at position: " << NumberPosition << endl;
+        cout << "The number found its order  : " << NumberPosition + 1 << endl;
+    }
+
+    return 0;
+
 }
